76111ba5f69b6ede0d000bc27f04f9d91932a340
Игорь Антипов <igorantivirus@yandex.ru>
2026-01-13 21:24:01 +0700

    Создана цена главного меню

---
+++ src/App/Scenes/MainMenuScene.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Core/Types.hpp"
+#include <Engine/Scene.hpp>
+
+class MainMenuScene : public engine::Scene
+{
+public:
+
+    inline static constexpr const IDType sceneID = 0;
+
+public:
+    
+    void updateEvent(const SDL_Event &event)
+    {
+
+    }
+    engine::SceneAction update(const float dt)
+    {
+        return engine::SceneAction::noneAction();
+    }
+
+    void draw(sdl3::RenderWindow &window) const
+    {
+
+    }
+
+private:
+
+
+
+
+};
