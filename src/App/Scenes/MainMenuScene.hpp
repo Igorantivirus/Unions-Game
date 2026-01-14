15c6959e6f1910af05350d34ef552a2e6f922b3d
Игорь Антипов <igorantivirus@yandex.ru>
2026-01-14 15:19:32 +0700

    Проверка на nullptr

--- src/App/Scenes/MainMenuScene.hpp
+++ src/App/Scenes/MainMenuScene.hpp
@@ -1,15 +1,30 @@
 #pragma once
 
+#include "Engine/AdvancedContext.hpp"
 #include <Core/Types.hpp>
 #include <Engine/Scene.hpp>
+#include <RmlUi/Core/ElementDocument.h>
 
 class MainMenuScene : public engine::Scene
 {
 public:
 
     inline static constexpr const IDType sceneID = 0;
+    inline static const std::string menuID = "main_menu";
 
 public:
+    MainMenuScene(engine::Context& context) : context_(context)
+    {
+        doc_ = context_.loadIfNoDocument("ui/MainMenu.html", menuID);
+        if (doc_)
+            doc_->Show();
+
+    }
+    ~MainMenuScene()
+    {
+        if (doc_)
+            doc_->Hide();
+    }
     
     void updateEvent(const SDL_Event &event)
     {
@@ -27,7 +42,8 @@ public:
 
 private:
 
-
+    engine::Context& context_;
+    Rml::ElementDocument* doc_ = nullptr;
 
 
 };
