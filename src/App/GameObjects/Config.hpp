54f36da6fd8b7d2bbcd632fa2bbc27083c168afb
Игорь Антипов <igorantivirus@yandex.ru>
2026-01-19 13:14:30 +0700

    Добавлены параметры для окружности

--- src/App/GameObjects/Config.hpp
+++ src/App/GameObjects/Config.hpp
@@ -4,7 +4,10 @@ namespace Config
 {
 inline constexpr const float PPM = 50.f; // Пикселей в метре
 inline constexpr const float MPP = 1.f / PPM;
+
 inline constexpr const float defaultDensity = 1.f;
+
 inline constexpr const float defaultFrictionRect = 0.3f;
 inline constexpr const float defaultFrictionEllipse = 0.5f;
+inline constexpr const float defaultFrictionCircle = defaultFrictionEllipse;
 } // namespace Config
