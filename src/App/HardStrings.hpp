dba9fe66a3efaa4d1e40f63b7987fd18c37b1aa4
Игорь Антипов <igorantivirus@yandex.ru>
2026-01-20 21:56:56 +0700

    Изменены хардстроки

--- src/App/HardStrings.hpp
+++ src/App/HardStrings.hpp
@@ -4,43 +4,36 @@
 
 namespace names
 {
-    constexpr const std::string_view mainIco = "ico.png";
-    constexpr const std::string_view statisticFile = "stat.xml";
-    constexpr const std::string_view windowName = "Объединялы";
+constexpr const std::string_view mainIco = "ico.png";
+constexpr const std::string_view statisticFile = "stat.xml";
+constexpr const std::string_view windowName = "Объединялы";
 
-}
+} // namespace names
 
 namespace assets
 {
-    constexpr const std::string_view packages = "objects";
-}
+constexpr const std::string_view packages = "objects";
+constexpr const std::string_view fontPath = "fonts/fonts.txt";
 
-namespace assets::fonts
-{
-    constexpr const std::string_view fontPath = "fonts/fonts.txt";
-}
+} // namespace assets
 
-namespace assets::ui
+namespace ui::mainMenu
 {
-    constexpr const std::string_view mainMenuRmlFile = "ui/MainMenu.html";
-    constexpr const std::string_view gameMenuRmlFile = "ui/GameMenu.html";
-    constexpr const std::string_view setsMenuRmlFile = "ui/SetsMenu.html";
-}
+constexpr const std::string_view file = "ui/MainMenu.html";
+constexpr const std::string_view exitB = "exit-b";
+constexpr const std::string_view startB = "start-b";
+constexpr const std::string_view settsB = "setts-b";
+} // namespace ui::mainMenu
 
-namespace assets::mainMenu
+namespace ui::gameMenu
 {
-    constexpr const std::string_view exitB = "exit-b";
-    constexpr const std::string_view startB = "start-b";
-    constexpr const std::string_view settsB = "setts-b";
-}
+constexpr const std::string_view file = "ui/GameMenu.html";
+constexpr const std::string_view backB = "back-b";
+} // namespace ui::gameMenu
 
-namespace assets::gameMenu
+namespace ui::setsMenu
 {
-    constexpr const std::string_view backB = "back-b";
-}
-
-namespace assets::gameMenu
-{
-    constexpr const std::string_view saveExitB = "save-exit-b";
-    constexpr const std::string_view saveThrowB = "throw-exit-b";
-}
+constexpr const std::string_view file = "ui/SetsMenu.html";
+constexpr const std::string_view saveExitB = "save-exit-b";
+constexpr const std::string_view saveThrowB = "throw-exit-b";
+} // namespace ui::gameMenu
