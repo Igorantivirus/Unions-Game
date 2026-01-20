de075ee39336e377b79dcdee9df19ba7ba671a27
Игорь Антипов <igorantivirus@yandex.ru>
2026-01-20 21:16:14 +0700

    Новые Core утилиты

---
+++ src/Core/PathMeneger.hpp
@@ -0,0 +1,52 @@
+#pragma once 
+
+#include <filesystem>
+
+namespace core
+{
+
+// PathManager.hpp - централизованное управление путями
+class PathManager
+{
+public:
+    static void init()
+    {
+        #ifdef ANDROID
+            assets = "";
+            workFolder = SDL_GetPrefPath("igorantivirus", "unions");
+        #else
+            #ifdef DEBUG_BUILD_TYPE
+                assets_ = "../assets";
+            #else
+                assets_ = "";
+            #endif
+            workFolder_ = "";
+        #endif
+    }
+
+    static const std::filesystem::path &assets()
+    {
+        return assets_;
+    }
+
+    static const std::filesystem::path &workFolder()
+    {
+        return workFolder_;
+    }
+
+    static const std::string inAssets(const std::string_view path)
+    {
+        return (assets_ / path).string();
+    }
+
+    static const std::string inWorkFolder(const std::string_view path)
+    {
+        return (workFolder_ / path).string();
+    }
+
+private:
+    inline static std::filesystem::path assets_;
+    inline static std::filesystem::path workFolder_;
+};
+
+} // namespace code
